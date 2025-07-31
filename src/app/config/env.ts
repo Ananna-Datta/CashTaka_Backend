05a0393e1fed78e0313eede55fb92fc21aea3d14
Ananna-Datta <anannadatta45@gmail.com>
2025-07-31 19:40:19 +0600

    Authentication Completed

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -1,28 +1,48 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-interface EnvConfig{
-    PORT:string,
-    DB_URL:string,
-    NODE_ENV:string,
-    BCRYPT_SALT_ROUND:number
+interface EnvConfig {
+  PORT: string;
+  DB_URL: string;
+  NODE_ENV: string;
+  BCRYPT_SALT_ROUND: number;
+  JWT_ACCESS_SECRET: string;
+  JWT_ACCESS_EXPIRES: string;
+  EXPRESS_SESSION_SECRET: string;
+  JWT_REFRESH_EXPIRES: string;
+  JWT_REFRESH_SECRET: string;
 }
 
-const loadEnvVariables = () :EnvConfig=>{
-    const requiredEnvVariables:string[]=["PORT","DB_URL","NODE_ENV","BCRYPT_SALT_ROUND"];
-    requiredEnvVariables.forEach(key=>{
-        if(!process.env[key]){
-            throw new Error(`MIssing require environment variable ${key}`)
-        }
-    })
-    return {
-    PORT:process.env.PORT as string,
-    DB_URL:process.env.DB_URL!,
-    NODE_ENV:process.env.NODE_ENV as "development" | "production",
+const loadEnvVariables = (): EnvConfig => {
+  const requiredEnvVariables: string[] = [
+    "PORT",
+    "DB_URL",
+    "NODE_ENV",
+    "BCRYPT_SALT_ROUND",
+    "JWT_ACCESS_SECRET",
+    "JWT_ACCESS_EXPIRES",
+    "EXPRESS_SESSION_SECRET",
+    "JWT_REFRESH_SECRET",
+    "JWT_REFRESH_EXPIRES"
+  ];
+  requiredEnvVariables.forEach((key) => {
+    if (!process.env[key]) {
+      throw new Error(`MIssing require environment variable ${key}`);
+    }
+  });
+  return {
+    PORT: process.env.PORT as string,
+    DB_URL: process.env.DB_URL!,
+    NODE_ENV: process.env.NODE_ENV as "development" | "production",
     BCRYPT_SALT_ROUND: Number(process.env.BCRYPT_SALT_ROUND!),
-}
-}
+    JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET!,
+    JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES!,
+    EXPRESS_SESSION_SECRET: process.env.EXPRESS_SESSION_SECRET!,
+    JWT_REFRESH_EXPIRES:process.env.JWT_REFRESH_EXPIRES!,
+    JWT_REFRESH_SECRET:process.env.JWT_REFRESH_SECRET!
+  };
+};
 
-export const envVars:EnvConfig = loadEnvVariables()
+export const envVars: EnvConfig = loadEnvVariables();
---
+++ src/app/modulers/auth/auth.controller.ts
@@ -0,0 +1,58 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { NextFunction, Request, Response } from "express"
+import httpStatus from "http-status-codes"
+import { catchAsync } from "../../utils/catchAsync"
+import { sendResponse } from "../../utils/sendResponse"
+import { AuthServices } from "./auth.service"
+import AppError from "../../errorhelper/appError"
+
+const credentialsLogin = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const loginInfo = await AuthServices.credentialsLogin(req.body)
+
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "User Logged In Successfully",
+        data: loginInfo,
+    })
+})
+const getNewAccessToken = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const refreshToken = req.cookies.refreshToken;
+    if (!refreshToken) {
+        throw new AppError(httpStatus.BAD_REQUEST, "No refresh token recieved from cookies")
+    }
+    const tokenInfo = await AuthServices.getNewAccessToken(refreshToken as string)
+
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "New Access Token Retrived Successfully",
+        data: tokenInfo,
+    })
+})
+const logout = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+
+    res.clearCookie("accessToken", {
+        httpOnly: true,
+        secure: false,
+        sameSite: "lax"
+    })
+    res.clearCookie("refreshToken", {
+        httpOnly: true,
+        secure: false,
+        sameSite: "lax"
+    })
+
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "User Logged Out Successfully",
+        data: null,
+    })
+})
+
+export const AuthControllers = {
+    credentialsLogin,
+    getNewAccessToken,
+    logout,   
+}
